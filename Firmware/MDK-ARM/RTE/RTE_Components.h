3b03823dff90f59f121802acfa40265184ad5e2c
janezjazbar <33789226+janezjazbar@users.noreply.github.com>
2017-12-17 12:05:52 +0100

    dodan file za PRCD procedure

---
+++ Firmware/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Welding_adapter' 
+ * Target:  'Welding_adapter' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
